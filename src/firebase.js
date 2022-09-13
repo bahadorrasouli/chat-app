048ac8395c60960b9499750e79ebd088bef8cadc
bahadorrasouli <100157692+bahadorrasouli@users.noreply.github.com>
2022-09-13 16:48:00 +0430

    change firebase setting

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyABsh5RciGSrX2iyjFKTnC1k0vzBEz2UVY",
-    authDomain: "botogram-ca064.firebaseapp.com",
-    projectId: "botogram-ca064",
-    storageBucket: "botogram-ca064.appspot.com",
-    messagingSenderId: "491761691080",
-    appId: "1:491761691080:web:622adb07974de4a59a78fd"
+    apiKey: "AIzaSyArJZwGKNqAQYfyqTZx0mz_YTHS6wTt0zI",
+    authDomain: "botogram-c2b70.firebaseapp.com",
+    projectId: "botogram-c2b70",
+    storageBucket: "botogram-c2b70.appspot.com",
+    messagingSenderId: "53258317",
+    appId: "1:53258317:web:a1174492d925f6facf5404"
   }).auth();
